bd0537ebe76e450a33a6801761d2ffdbe3519ef4
Voin <voinekkx@gmail.com>
2022-01-07 21:14:04 +0100

    Version 0.2, remade variables names and created current framework bar

--- resources/scripts/main.js
+++ resources/scripts/main.js
@@ -1,8 +1,25 @@
 "use strict";
-//HTML variables
+const VariablesJQuery = {
+    intro : $('#intro'), 
+    exitPopup : $('#exit-popup'),
+    popup : $('#popup'),
+    textPopup : $('#text-popup'),
+    loginRegisterButton : $('#login-register-button'),
+    login : $('#login'),
+    loginRegisterSwitch : $('#login-register-switch'),
+    confirmPassword : $('#confirm-password'),
+    password : $('#password'),
+    password2 : $('#password2'),
+    container : $('#container'),
+    containerContent : $('#container-content'),
+    containerContentMenu : $('#container-content2'),
+    currentFramework: $('#framework'),
+}
+
+VariablesJQuery.currentFramework.text('jQuery');
 
 $(document).ready(()=>{
-    //Intro text writting effect
+    //Intro text and writing effect function
     const writingEffect = ()=>{
         let text = "Hello World!";
         let writtenText = "H";
@@ -12,7 +29,7 @@ $(document).ready(()=>{
             writtenText += text[lettersWritten];
             lettersWritten++;
             if(lettersWritten<=text.length){
-                $('#intro').text(writtenText);
+                VariablesJQuery.intro.text(writtenText);
             }
             else clearInterval(writingInterval);
         },150);
@@ -24,75 +41,75 @@ $(document).ready(()=>{
         PopupSettings.position.left=window.innerWidth/2;
     });
 
-    $('#exitPopup').on('click',()=>{ //exit button in popup functionality
-        $('#popup').fadeOut(500);
+    VariablesJQuery.exitPopup.on('click',()=>{ //exit button in popup functionality
+        VariablesJQuery.popup.fadeOut(500);
         removeEventListener('resize', ()=>{ // memory clear
-            $('#popup').css('top', PopupSettings.position.top);
-            $('#popup').css('left', PopupSettings.position.left);
+            VariablesJQuery.popup.css('top', PopupSettings.position.top);
+            VariablesJQuery.popup.css('left', PopupSettings.position.left);
         })
     })
 
     const Popup = {
         show : (content,time) => {//popup show function
-            $('#popupText').text(content);
-            $('#popup').css('top', PopupSettings.position.top);
-            $('#popup').css('left', PopupSettings.position.left);
-            $('#popup').show();
+            VariablesJQuery.textPopup.text(content);
+            VariablesJQuery.popup.css('top', PopupSettings.position.top);
+            VariablesJQuery.popup.css('left', PopupSettings.position.left);
+            VariablesJQuery.popup.show();
             setTimeout(()=>{
-                $('#popup').fadeOut(500);
+                VariablesJQuery.popup.fadeOut(500);
             },time);
         },
         resize : (time) => { // popup change position on resize
             addEventListener('resize', ()=>{
-                $('#popup').css('top', PopupSettings.position.top);
-                $('#popup').css('left', PopupSettings.position.left);
+                VariablesJQuery.popup.css('top', PopupSettings.position.top);
+                VariablesJQuery.popup.css('left', PopupSettings.position.left);
             })
             setTimeout(()=>{
                 removeEventListener('resize', ()=>{ // memory clear
-                    $('#popup').css('top', PopupSettings.position.top);
-                    $('#popup').css('left', PopupSettings.position.left);
+                    VariablesJQuery.popup.css('top', PopupSettings.position.top);
+                    VariablesJQuery.popup.css('left', PopupSettings.position.left);
                 })
             },time)
         }
     }
 
-    $('#loginBtn').on('click', ()=>{ //login button functionality
+    VariablesJQuery.loginRegisterButton.on('click', ()=>{ //login button functionality
         //verification of login and pass
-        if($('#login').val().length>=LoginCredentials.maxLoginLength-1 || $('#login').val().length<=LoginCredentials.minLoginLength-1){
+        if(VariablesJQuery.login.val().length>=LoginCredentials.maxLoginLength-1 || VariablesJQuery.login.val().length<=LoginCredentials.minLoginLength-1){
             Popup.show(`Login needs to be ${LoginCredentials.minLoginLength}-${LoginCredentials.maxLoginLength} characters long!`, PopupSettings.time);
             Popup.resize(PopupSettings.time);
         }
-        else if ($('#pass').val().length<=LoginCredentials.minPasswordLength-1 || $('#pass').val().length>=LoginCredentials.maxPasswordLength-1){
+        else if (VariablesJQuery.password.val().length<=LoginCredentials.minPasswordLength-1 || VariablesJQuery.password.val().length>=LoginCredentials.maxPasswordLength-1){
             Popup.show(`Password needs to be ${LoginCredentials.minPasswordLength}-${LoginCredentials.maxPasswordLength} characters long!`, PopupSettings.time);
             Popup.resize(PopupSettings.time);
         }
-        else if (menuStatus=='register' && $('#pass').val()!=$('#pass2').val()){
+        else if (menuStatus=='register' && VariablesJQuery.password.val()!=VariablesJQuery.password2.val()){
             Popup.show(`Passwords doesn't match!`, PopupSettings.time);
             Popup.resize(PopupSettings.time);
         }
         else {
-            $('#container').addClass('scretch');
-            $('#container-content').fadeOut(500);
+            VariablesJQuery.container.addClass('scretch');
+            VariablesJQuery.containerContent.fadeOut(500);
             setTimeout(()=>{
-                $('#container-content2').fadeIn(500);
+                VariablesJQuery.containerContentMenu.fadeIn(500);
             },500)
             
         }
     })
 
-    $('#registerBtn').on('click', ()=>{ //register button functionality
+    VariablesJQuery.loginRegisterSwitch.on('click', ()=>{ //register button functionality
         if(menuStatus=='login'){
-            $('#pass2').slideDown(500);
-            $('#confirmPass').slideDown(500);
-            $('#registerBtn').text('Already have account? Log In!');
-            $('#loginBtn').text('Register');
+            VariablesJQuery.password2.slideDown(500);
+            VariablesJQuery.confirmPassword.slideDown(500);
+            VariablesJQuery.loginRegisterSwitch.text('Already have account? Log In!');
+            VariablesJQuery.loginRegisterButton.text('Register');
             menuStatus='register';
         }
         else {
-            $('#pass2').slideUp(500);
-            $('#confirmPass').slideUp(500);
-            $('#registerBtn').text('Not registered?');
-            $('#loginBtn').text('Log In');
+            VariablesJQuery.password2.slideUp(500);
+            VariablesJQuery.confirmPassword.slideUp(500);
+            VariablesJQuery.loginRegisterSwitch.text('Not registered?');
+            VariablesJQuery.loginRegisterButton.text('Log In');
             menuStatus='login';
         }
     })
