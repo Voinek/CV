3e6d46045411d865bf883c7c21ae6a2c2db4536d
Voinek <voinekkx@gmail.com>
2022-02-21 16:32:52 +0100

    Version 0.4, added fully functional meditate app

--- resources/scripts/applications/meditate.js
+++ resources/scripts/applications/meditate.js
@@ -1,7 +1,120 @@
-class Meditate {
-    constructor() {
-        $('#app-space').addClass('appMeditateOn');
-        console.log('Class Meditate/constructor running!');
+{
+    const buttons = document.querySelectorAll('.meditateButtons');
+    const body = document.body;
+    const startStop = document.querySelector('.pauseRenewButton');
+    const appBox = document.querySelector('.appSpaceMeditate');
+    let time = 5 * 60;
+    let theme = 1;
+    let buttonStatus = true;
+    let audio = document.createElement('audio');
+    let timePassedInterval;
+
+    appBox.addEventListener('mouseover', () => {
+        appBox.classList.remove('appBoxActive');
+    })
+    appBox.addEventListener('mouseout', () => {
+        appBox.classList.add('appBoxActive');
+    })
+
+    buttons[0].addEventListener('click', () => {
+        time = 5 * 60;
+        borderClearTime();
+        buttons[0].style.border = 'solid grey 3px';
+    })
+    buttons[1].addEventListener('click', () => {
+        time = 10 * 60;
+        borderClearTime();
+        buttons[1].style.border = 'solid grey 3px';
+    })
+    buttons[2].addEventListener('click', () => {
+        time = 30 * 60;
+        borderClearTime();
+        buttons[2].style.border = 'solid grey 3px';
+    })
+    buttons[3].addEventListener('click', () => {
+        theme = 1;
+        borderClearTheme();
+        buttons[3].style.border = 'solid grey 3px';
+    })
+    buttons[4].addEventListener('click', () => {
+        theme = 2;
+        borderClearTheme();
+        buttons[4].style.border = 'solid grey 3px';
+    })
+    buttons[5].addEventListener('click', () => {
+        theme = 3;
+        borderClearTheme();
+        buttons[5].style.border = 'solid grey 3px';
+    })
+
+
+    const meditateApp = () => {
+        buttonStatus = !buttonStatus;
+        if (buttonStatus == false) {
+            startStop.innerHTML = '<h2>Stop</h2>'
+            switch (theme) {
+                case 1:
+                    body.style.backgroundImage = 'url(../resources/images/campfire.jpg)'
+                    sound('../resources/sounds/campfire.mp3');
+                    countTime(time);
+                    break;
+                case 2:
+                    body.style.backgroundImage = 'url(../resources/images/forest.jpg)';
+                    sound('../resources/sounds/forest.mp3');
+                    countTime(time);
+                    break;
+                case 3:
+                    body.style.backgroundImage = 'url(../resources/images/sea.jpg)'
+                    sound('../resources/sounds/sea.mp3');
+                    countTime(time);
+                    break;
+                default:
+                    console.log('error, meditateApp theme != 1,2,3');
+                    break;
+            }
+        } else {
+            startStop.innerHTML = '<h2>Start</h2>'
+            audio.src = "";
+            clearInterval(timePassedInterval);
+        }
+    }
+    startStop.addEventListener('click', () => {
+        meditateApp()
+    })
+
+    const borderClearTheme = () => {
+        document.querySelectorAll('.themeButton').forEach(item => {
+            item.style.border = 'solid black 0px';
+        })
+    }
+    const borderClearTime = () => {
+        document.querySelectorAll('.timeButton').forEach(item => {
+            item.style.border = 'solid black 0px';
+        })
+    }
+    const sound = url => {
+        audio.style.display = 'none';
+        audio.src = url;
+        audio.autoplay = true
+        audio.onended = () => audio.remove();
+        document.body.appendChild(audio);
+    }
+    const countTime = (maxTime) => {
+        let time = maxTime;
+        timePassed = 0;
+        timePassedInterval = setInterval(() => {
+            if (timePassed < maxTime) {
+                timePassed++;
+                document.querySelector('.progressBar').style.strokeDashoffset = `calc(440 - (440 * ${timePassed / time * 100}) / 100)`;
+            } else {
+                clearInterval(timePassedInterval);
+                timePassed = 0;
+            }
+        }, 1000)
+    }
+    var appOffMeditate = () => {
+        clearInterval(timePassedInterval);
+        body.style.backgroundImage = "none";
+        appBox.style.display = "none";
     }
 }
-// nie ma sensu tworzenia takiej klasy
--- resources/scripts/main.js
+++ resources/scripts/main.js
@@ -1,18 +1,18 @@
 "use strict";
 const VariablesJQuery = {
-    intro : $('#intro'), 
-    exitPopup : $('#exit-popup'),
-    popup : $('#popup'),
-    textPopup : $('#text-popup'),
-    loginRegisterButton : $('#login-register-button'),
-    login : $('#login'),
-    loginRegisterSwitch : $('#login-register-switch'),
-    confirmPassword : $('#confirm-password'),
-    password : $('#password'),
-    password2 : $('#password2'),
-    container : $('#container'),
-    containerContent : $('#container-content'),
-    containerContentMenu : $('#container-content2'),
+    intro: $('#intro'),
+    exitPopup: $('#exit-popup'),
+    popup: $('#popup'),
+    textPopup: $('#text-popup'),
+    loginRegisterButton: $('#login-register-button'),
+    login: $('#login'),
+    loginRegisterSwitch: $('#login-register-switch'),
+    confirmPassword: $('#confirm-password'),
+    password: $('#password'),
+    password2: $('#password2'),
+    container: $('#container'),
+    containerContent: $('#container-content'),
+    containerContentMenu: $('#container-content2'),
     currentFramework: $('#framework'),
     menuStageOne: $('.menu-appear-stage-one'),
     menuStageTwo: $('.menu-appear-stage-two'),
@@ -21,143 +21,151 @@ const VariablesJQuery = {
     burgerMenu: $('#burger-menu-button'),
 }
 
-$(document).ready(()=>{
+$(document).ready(() => {
     //Intro text and writing effect function
-    const writingEffect = ()=>{
+    const writingEffect = () => {
         let text = "Hello World!";
         let writtenText = "H";
         let lettersWritten = 1;
-    
-        const writingInterval = setInterval(()=>{
+
+        const writingInterval = setInterval(() => {
             writtenText += text[lettersWritten];
             lettersWritten++;
-            if(lettersWritten<=text.length){
+            if (lettersWritten <= text.length) {
                 VariablesJQuery.intro.text(writtenText);
-            }
-            else clearInterval(writingInterval);
-        },150);
+            } else clearInterval(writingInterval);
+        }, 150);
     }
     writingEffect();
 
-    addEventListener('resize', ()=>{ //
-        PopupSettings.position.top=window.innerHeight/2;
-        PopupSettings.position.left=window.innerWidth/2;
+    addEventListener('resize', () => { //
+        PopupSettings.position.top = window.innerHeight / 2;
+        PopupSettings.position.left = window.innerWidth / 2;
     });
 
-    VariablesJQuery.exitPopup.on('click',()=>{ //exit button in popup functionality
+    VariablesJQuery.exitPopup.on('click', () => { //exit button in popup functionality
         VariablesJQuery.popup.fadeOut(500);
-        removeEventListener('resize', ()=>{ // memory clear
+        removeEventListener('resize', () => { // memory clear
             VariablesJQuery.popup.css('top', PopupSettings.position.top);
             VariablesJQuery.popup.css('left', PopupSettings.position.left);
         })
     })
 
     const Popup = {
-        show : (content,time) => {//popup show function
+        show: (content, time) => { //popup show function
             VariablesJQuery.textPopup.text(content);
             VariablesJQuery.popup.css('top', PopupSettings.position.top);
             VariablesJQuery.popup.css('left', PopupSettings.position.left);
             VariablesJQuery.popup.show();
-            setTimeout(()=>{
+            setTimeout(() => {
                 VariablesJQuery.popup.fadeOut(500);
-            },time);
+            }, time);
         },
-        resize : (time) => { // popup change position on resize
-            addEventListener('resize', ()=>{
+        resize: (time) => { // popup change position on resize
+            addEventListener('resize', () => {
                 VariablesJQuery.popup.css('top', PopupSettings.position.top);
                 VariablesJQuery.popup.css('left', PopupSettings.position.left);
             })
-            setTimeout(()=>{
-                removeEventListener('resize', ()=>{ // memory clear
+            setTimeout(() => {
+                removeEventListener('resize', () => { // memory clear
                     VariablesJQuery.popup.css('top', PopupSettings.position.top);
                     VariablesJQuery.popup.css('left', PopupSettings.position.left);
                 })
-            },time)
+            }, time)
         }
     }
 
-    VariablesJQuery.loginRegisterButton.on('click', ()=>{ //login button functionality
+    VariablesJQuery.loginRegisterButton.on('click', () => { //login button functionality
         //verification of login and pass
-        if(VariablesJQuery.login.val().length>=LoginCredentials.maxLoginLength-1 || VariablesJQuery.login.val().length<=LoginCredentials.minLoginLength-1){
+        if (VariablesJQuery.login.val().length >= LoginCredentials.maxLoginLength - 1 || VariablesJQuery.login.val().length <= LoginCredentials.minLoginLength - 1) {
             Popup.show(`Login needs to be ${LoginCredentials.minLoginLength}-${LoginCredentials.maxLoginLength} characters long!`, PopupSettings.time);
             Popup.resize(PopupSettings.time);
-        }
-        else if (VariablesJQuery.password.val().length<=LoginCredentials.minPasswordLength-1 || VariablesJQuery.password.val().length>=LoginCredentials.maxPasswordLength-1){
+        } else if (VariablesJQuery.password.val().length <= LoginCredentials.minPasswordLength - 1 || VariablesJQuery.password.val().length >= LoginCredentials.maxPasswordLength - 1) {
             Popup.show(`Password needs to be ${LoginCredentials.minPasswordLength}-${LoginCredentials.maxPasswordLength} characters long!`, PopupSettings.time);
             Popup.resize(PopupSettings.time);
-        }
-        else if (menuStatus=='register' && VariablesJQuery.password.val()!=VariablesJQuery.password2.val()){
+        } else if (menuStatus == 'register' && VariablesJQuery.password.val() != VariablesJQuery.password2.val()) {
             Popup.show(`Passwords doesn't match!`, PopupSettings.time);
             Popup.resize(PopupSettings.time);
-        }
-        else {
+        } else {
             animateMenu(AnimationSettings.durationTime, AnimationSettings.delayTime);
         }
     })
 
-    VariablesJQuery.loginRegisterSwitch.on('click', ()=>{ //"registered?" button functionality
-        if(menuStatus==='login'){
+    VariablesJQuery.loginRegisterSwitch.on('click', () => { //"registered?" button functionality
+        if (menuStatus === 'login') {
             VariablesJQuery.password2.slideDown(500);
             VariablesJQuery.confirmPassword.slideDown(500);
             VariablesJQuery.loginRegisterSwitch.text('Already have account? Log In!');
             VariablesJQuery.loginRegisterButton.text('Register');
-            menuStatus='register';
-        }
-        else {
+            menuStatus = 'register';
+        } else {
             VariablesJQuery.password2.slideUp(500);
             VariablesJQuery.confirmPassword.slideUp(500);
             VariablesJQuery.loginRegisterSwitch.text('Not registered?');
             VariablesJQuery.loginRegisterButton.text('Log In');
-            menuStatus='login';
+            menuStatus = 'login';
         }
     })
 });
 
 //menu buttons
-VariablesJQuery.burgerMenu.on('click', ()=>{ // Hide burger menu and show menu again
-    animateBurgerMenu(500,0);
+VariablesJQuery.burgerMenu.on('click', () => { // Hide burger menu and show menu again
+    animateBurgerMenu(500, 0);
 })
 
 document.querySelectorAll("#container-content2 p:nth-child(n+2)").forEach((item, index) => { // add "hide side menu" animation to every item in menu
-    item.addEventListener('click', ()=>{
-        animateHideMenu(500,500);
-        switch(index){ // button redirecting to another js file
+    item.addEventListener('click', () => {
+        animateHideMenu(500, 500);
+        switch (index) { // button redirecting to another js file
             case 0:
-                const meditate = new Meditate();
-            break;
+                appOffMeditate();
+                $('.appSpaceMeditate').css("display", "inline-flex");
+                VariablesJQuery.currentFramework.text('None');
+                VariablesJQuery.containerContentMenu.css("color", "white")
+                break;
             case 1:
+                appOffMeditate();
 
-            break;
+                break;
             case 2:
+                appOffMeditate();
 
-            break;
+                break;
             case 3:
+                appOffMeditate();
 
-            break;
+                break;
             case 4:
+                appOffMeditate();
 
-            break;
+                break;
             case 5:
+                appOffMeditate();
 
-            break;
+                break;
             case 6:
+                appOffMeditate();
 
-            break;
+                break;
             case 7:
+                appOffMeditate();
 
-            break;
+                break;
             case 8:
+                appOffMeditate();
 
-            break;
+                break;
             case 9:
+                appOffMeditate();
 
-            break;
+                break;
             default:
-                
-            break;
+
+                break;
         }
     })
 })
 
-
-
+const turnOffAll = () => {
+    appOffMeditate();
+}
